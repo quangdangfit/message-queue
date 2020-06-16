85864fb4948f886fcef3504d1d3d773978d0af04
Quang Đặng <quangdangfit@gmail.com>
2020-06-16 15:46:56 +0700

    add cron resend to send wait out messages

---
+++ cmd/cron/resend.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"gomq/dbs"
+	"gomq/msgQueue"
+	"lib/utils/logger"
+)
+
+const ResendOutMessageLimit = 100
+
+func main() {
+	messages, _ := dbs.GetWaitOutMessage(ResendOutMessageLimit)
+	if messages == nil {
+		logger.Info("Not found any wait message!")
+		return
+	}
+
+	pub := msgQueue.NewPublisher(false)
+	for _, msg := range messages {
+		err := pub.Publish(&msg, true)
+
+		if err != nil {
+			logger.Error("Failed to resend msg: ", msg.RoutingKey, msg.OriginModel, msg.OriginCode, err)
+			msg.Logs = err.Error()
+			msg.Status = dbs.OutMessageStatusFailed
+		} else {
+			msg.Status = dbs.OutMessageStatusSent
+		}
+
+		err = dbs.UpdateOutMessage(&msg)
+		if err != nil {
+			logger.Errorf("Sent, failed to update status: %s, %s, %s, error: %s", msg.RoutingKey, msg.OriginModel, msg.OriginCode, err)
+			continue
+		}
+	}
+}
--- dbs/outMessage.go
+++ dbs/outMessage.go
@@ -2,6 +2,8 @@ package dbs
 
 import (
 	"gomq/models"
+	"gopkg.in/mgo.v2/bson"
+	"time"
 )
 
 const (
@@ -13,10 +15,47 @@ const (
 	OutMessageStatusInvalid  = "invalid"
 )
 
+func GetWaitOutMessage(limit int) ([]models.OutMessage, error) {
+	message := []models.OutMessage{}
+	query := bson.M{"status": "wait"}
+
+	_, err := Database.FindManyPaging(CollectionOutMessage, query, "-_id", 1,
+		limit, &message)
+	if err != nil {
+		return nil, err
+	}
+
+	return message, nil
+}
+
 func AddOutMessage(message *models.OutMessage) (*models.OutMessage, error) {
 	err := Database.InsertOne(CollectionOutMessage, message)
 	if err != nil {
 		return nil, err
 	}
 	return message, nil
+}
+
+func UpdateOutMessage(message *models.OutMessage) error {
+	//TODO: create field id (uuid) and query by id
+	selector := bson.M{
+		"routing_key":  message.RoutingKey,
+		"origin_model": message.OriginModel,
+		"routing_code": message.OriginCode,
+		"create_time":  message.CreatedTime,
+	}
+
+	payload := map[string]interface{}{
+		"updated_time": time.Now(),
+		"status":       message.Status,
+		"logs":         message.Logs,
+	}
+
+	change := bson.M{"$set": payload}
+	err := Database.UpdateOne(CollectionOutMessage, selector, change)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
