ca8158f1f5527fd9f6e24c7b97113cd90eec3e16
Quang Dang <quangdangfit@gmail.com>
2020-08-31 11:36:14 +0700

    remove create index no longer need

--- app/dbs/dbs.go
+++ app/dbs/dbs.go
@@ -3,9 +3,7 @@ package dbs
 import (
 	"github.com/quangdangfit/gosdk/database"
 	"github.com/quangdangfit/gosdk/database/mongo"
-	"gopkg.in/mgo.v2"
 
-	"message-queue/app/models"
 	"message-queue/config"
 )
 
@@ -27,52 +25,4 @@ func NewDatabase() IDatabase {
 	}
 
 	return mongo.New(dbConfig)
-}
-
-func ensureIndex() {
-	// Index for OutMessages
-	indexOutId := mgo.Index{
-		Key:        []string{"id"},
-		Unique:     true,
-		DropDups:   false,
-		Background: true,
-		Sparse:     true,
-		Name:       "out_message_id_index",
-	}
-	Database.DropIndex(models.CollectionOutMessage, indexOutId.Name)
-	Database.EnsureIndex(models.CollectionOutMessage, indexOutId)
-
-	indexOutObj := mgo.Index{
-		Key:        []string{"origin_id, origin_model"},
-		Unique:     false,
-		DropDups:   false,
-		Background: true,
-		Sparse:     true,
-		Name:       "origin_object_index",
-	}
-	Database.DropIndex(models.CollectionOutMessage, indexOutObj.Name)
-	Database.EnsureIndex(models.CollectionOutMessage, indexOutObj)
-
-	// Index for InMessages
-	indexInId := mgo.Index{
-		Key:        []string{"id"},
-		Unique:     true,
-		DropDups:   false,
-		Background: true,
-		Sparse:     true,
-		Name:       "in_message_id_index",
-	}
-	Database.DropIndex(models.CollectionInMessage, indexInId.Name)
-	Database.EnsureIndex(models.CollectionInMessage, indexInId)
-
-	indexInObj := mgo.Index{
-		Key:        []string{"origin_id, origin_model"},
-		Unique:     false,
-		DropDups:   false,
-		Background: true,
-		Sparse:     true,
-		Name:       "origin_object_index",
-	}
-	Database.DropIndex(models.CollectionInMessage, indexInObj.Name)
-	Database.EnsureIndex(models.CollectionInMessage, indexInObj)
 }
